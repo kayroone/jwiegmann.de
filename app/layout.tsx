646a53adaf62f532f3fd756799058097793dcbf2
jan.wiegmann <jw@jwiegmann.de>
2025-03-18 08:32:44 +0100

    deploy.yml angepasst / kleine Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css"
 import { Inter } from "next/font/google"
-import type React from "react" // Import React
+import type React from "react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -16,10 +16,4 @@ export default function RootLayout({
   )
 }
 
-
-
 import './globals.css'
-
-export const metadata = {
-      generator: 'v0.dev'
-    };
