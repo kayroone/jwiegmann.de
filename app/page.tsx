ca00e33c31ab5a74986ac07ed2405d7c1b220d4c
jan.wiegmann <jw@jwiegmann.de>
2025-03-20 08:47:30 +0100

    Rename

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Hero from "./components/hero"
+import MainPage from "./components/hero"
 import BlogPreview from "./components/blog-preview"
 import Footer from "./components/footer"
 
 export default function Page() {
   return (
     <main className="min-h-screen bg-black text-white">
-      <Hero />
+      <MainPage />
       <BlogPreview />
       <Footer />
     </main>
