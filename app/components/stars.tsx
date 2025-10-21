4bcf1aef017d6dacffddb3fde6446069a1da0937
jan.wiegmann <jw@jwiegmann.de>
2025-10-21 14:11:33 +0200

    Space invader reveal time fix

--- app/components/stars.tsx
+++ app/components/stars.tsx
@@ -146,7 +146,7 @@ export default function Stars() {
 
     // Spawn invaders periodically
     let lastInvaderSpawn = Date.now()
-    const invaderSpawnInterval = 4000 // spawn every 4 seconds
+    const invaderSpawnInterval = 2000 // spawn every 2 seconds
 
     function animate() {
       if (!ctx) return
